80a1df6d1dcdb469da85d903b6f6f5ff1b13a273
Jhonathan Perdomo <perdomoj@inlaca.local>
2019-11-20 10:33:17 -0400

    reparaciones

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\FormServiceProvider::class,
+       // App\Providers\FormServiceProvider::class,
 
     ],
 
@@ -227,8 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+      /*  'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,*/
 
     ],
 
